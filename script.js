76092943f07cab2ef30b260c63766da914ad5f7c
EweLira <113145267+EweLira@users.noreply.github.com>
2023-05-26 19:38:17 -0300

    Adicionando o localhost

--- script.js
+++ script.js
@@ -1,93 +1,105 @@
 var player1Score = 0;
-		var player2Score = 0;
-		var currentPlayer = 1;
-		var currentRound = 1;
-        function playDice(player) {
-		if (currentRound <= 10) {
-			if (player === currentPlayer) {
-				var diceResult = Math.floor(Math.random() * 6) + 1;
-				document.getElementById("message").innerHTML = "Jogador " + player + " jogou o dado e tirou " + diceResult + ".";
-				if (player === 1) {
-					player1Score += diceResult;
-				} else {
-					player2Score += diceResult;
-				}
-				currentPlayer = (currentPlayer === 1) ? 2 : 1;
-				if (currentPlayer === 1) {
-					currentRound++;
-					showScore();
-					checkWinner();
-				}
-			} else {
-				document.getElementById("message").innerHTML = "Não é a vez do Jogador " + player + ".";
-			}
-		}
-	}
+var player2Score = 0;
+var currentPlayer = 1;
+var currentRound = 1;
+var resultData = [];
 
-	function checkWinner() {
-		if (currentRound <= 10) {
-			if (currentPlayer === 1) {
-				if (player1Score > player2Score) {
-					document.getElementById("message").innerHTML += " Jogador 1 venceu a rodada " + currentRound + "!";
-				} else if (player1Score < player2Score) {
-					document.getElementById("message").innerHTML += " Jogador 2 venceu a rodada " + currentRound + "!";
-				} else {
-					document.getElementById("message").innerHTML += " Empate na rodada " + currentRound + "!";
-				}
-			}
-		}
-	}
+function restoreGame() {
+  var savedGame = localStorage.getItem("gameState");
+  if (savedGame) {
+    var gameState = JSON.parse(savedGame);
+    Object.assign(this, gameState);
+    rebuildResultTable();
+    showScore();
+  }
+}
 
-	function showScore() {
-		document.getElementById("score").innerHTML = "Placar: Jogador 1 - " + player1Score + " / Jogador 2 - " + player2Score;
-	}
+function saveGame() {
+  var gameState = {
+    player1Score,
+    player2Score,
+    currentPlayer,
+    currentRound,
+    resultData
+  };
+  localStorage.setItem("gameState", JSON.stringify(gameState));
+}
 
-    function updateResultTable() {
-  var resultTable = document.getElementById("result-table");
-  var newRow = resultTable.insertRow(-1);
-  var roundCell = newRow.insertCell(0);
-  var player1Cell = newRow.insertCell(1);
-  var player2Cell = newRow.insertCell(2);
-  var winnerCell = newRow.insertCell(3);
-
-  roundCell.innerHTML = currentRound;
-  player1Cell.innerHTML = player1Score;
-  player2Cell.innerHTML = player2Score;
-
-  if (player1Score > player2Score) {
-    winnerCell.innerHTML = "Jogador 1";
-  } else if (player1Score < player2Score) {
-    winnerCell.innerHTML = "Jogador 2";
+function playDice(player) {
+  if (player === currentPlayer) {
+    var diceResult = Math.floor(Math.random() * 6) + 1;
+    document.getElementById("message").innerHTML = `Jogador ${player} jogou o dado e tirou ${diceResult}.`;
+    this[`player${player}Score`] += diceResult;
+    currentPlayer = currentPlayer === 1 ? 2 : 1;
+    if (currentPlayer === 1) {
+      currentRound++;
+      showScore();
+      checkWinner();
+      saveRoundData();
+      saveGame();
+    }
   } else {
-    winnerCell.innerHTML = "Empate";
+    document.getElementById("message").innerHTML = `Não é a vez do Jogador ${player}.`;
   }
 }
 
 function checkWinner() {
-    if (currentRound <= 10) {
-      if (currentPlayer === 1) {
-        if (player1Score > player2Score) {
-          document.getElementById("message").innerHTML += " Jogador 1 venceu a rodada " + currentRound + "!";
-        } else if (player1Score < player2Score) {
-          document.getElementById("message").innerHTML += " Jogador 2 venceu a rodada " + currentRound + "!";
-        } else {
-          document.getElementById("message").innerHTML += " Empate na rodada " + currentRound + "!";
-        }
-        updateResultTable();
-      }
-    }
+  if (currentPlayer === 1) {
+    var winnerMsg = player1Score > player2Score ? "Jogador 1" : player1Score < player2Score ? "Jogador 2" : "Empate";
+    document.getElementById("message").innerHTML += ` ${winnerMsg} venceu a rodada ${currentRound}!`;
+    rebuildResultTable();
   }
-  function reiniciar() {
-    var table = document.getElementById("tabela");
-    table.innerHTML = "";
 }
-  
-	function resetGame() {
-		player1Score = 0;
-		player2Score = 0;
-		currentPlayer = 1;
-		currentRound = 1;
-		showScore();
-		document.getElementById("message").innerHTML = "";
-	}
-	showScore();
+
+function showScore() {
+  document.getElementById("score").innerHTML = `Placar: Jogador 1 - ${player1Score} / Jogador 2 - ${player2Score}`;
+}
+
+function saveRoundData() {
+  resultData.push({
+    round: currentRound,
+    player1Score,
+    player2Score,
+    winner: player1Score > player2Score ? "Jogador 1" : player1Score < player2Score ? "Jogador 2" : "Empate"
+  });
+}
+
+function rebuildResultTable() {
+  var resultTable = document.getElementById("result-table");
+  resultTable.innerHTML = "<tr><th>Rodada</th><th>Jogador 1</th><th>Jogador 2</th><th>Vencedor</th></tr>";
+
+  resultData.forEach(function(data) {
+    var newRow = resultTable.insertRow(-1);
+    newRow.innerHTML = `<td>${data.round}</td><td>${data.player1Score}</td><td>${data.player2Score}</td><td>${data.winner}</td>`;
+  });
+}
+
+function resetGame() {
+  player1Score = 0;
+  player2Score = 0;
+  currentPlayer = 1;
+  currentRound = 1;
+  resultData = [];
+  showScore();
+  document.getElementById("message").innerHTML = "";
+  resetTable();
+  saveGame();
+}
+
+function resetGame() {
+  player1Score = 0;
+  player2Score = 0;
+  currentPlayer = 1;
+  currentRound = 1;
+  resultData = [];
+  showScore();
+  document.getElementById("message").innerHTML = "";
+  resetTable();
+  saveGame();
+}
+
+function resetTable() {
+  var resultTable = document.getElementById("result-table");
+  resultTable.innerHTML = "<tr><th>Rodada</th><th>Jogador 1</th><th>Jogador 2</th><th>Vencedor</th></tr>";
+}
+
